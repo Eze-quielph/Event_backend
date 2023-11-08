26eac739fe35778d809d30178d892e11e69098d6
eze-quielph <ezequiel.fotografiaa@gmail.com>
2023-11-08 13:27:55 -0300

    public access upcoming

--- src/Module/user/user.controller.ts
+++ src/Module/user/user.controller.ts
@@ -14,11 +14,7 @@ export class UserController {
   async register(@Body() createUserDto: CreateUserDto) {
     try {
       
-<<<<<<< Updated upstream
-        const currentDate = new Date()
-=======
       const currentDate = new Date()
->>>>>>> Stashed changes
       const dataUser = new Date(createUserDto.Birthdate)
       
       if (dataUser > currentDate) {
